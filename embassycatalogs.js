6117796ee6de0c3acef3444cddbf7fcf6d18d8ce
Kurollos <144329418+Kurollos@users.noreply.github.com>
2025-11-20 12:04:33 +0100

    ik heb de js file verwijdert omdat die niet werkt #16
    ik heb hert verwijdert omdat ik het niet nodig vindt en dat de extra nummers wel werken

--- embassycatalogs.js
+++
@@ -1,9 +0,0 @@
- // Eenvoudige JS logica voor min/plus buttons
-    function changeQuantity(amount) {
-      const input = document.getElementById('quantity');
-      let value = parseInt(input.value) || 0;
-      value += amount;
-      if (value < 1) value = 1;
-      input.value = value;
-    }
-
